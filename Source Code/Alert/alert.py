128605156356d59142b0be73515e693e559eeb98
Mahdi Mahajni <mahdegamer76@gmail.com>
2025-05-09 23:10:58 +0300

    Intial Implmentation for alert

--- Source Code/Alert/alert.py
+++ Source Code/Alert/alert.py
@@ -1,9 +1,23 @@
-def alert(capture, item, face, time):
-    # Write to a JSON file about the event.
-        # capture: path to the recording of the theft
-        # item: 
-            # Item_Name
-            # Item Price
-        # face: path to the image of the theif
-        # time: time of theft
-    pass
+import json
+from datetime import datetime  
+def alert(theft_link, item, face, time):    
+        theftalert={
+            "theft link":theft_link,
+            "item":item,
+            "face ID":face,
+            "theft time": time
+        }
+        path=r"./Data/Alert.json"
+        with open(path,"w") as json_file:json.dump(theftalert,json_file,indent=4)
+pass
+        
+alert( 
+        theft_link=r"D:\FinalProject\Final-BS.C-Project-2025\Data\Stream\stream.mp4",
+        item=[
+                {"Item_Name": "banana","Item Price":" 11$ " },
+                {"Item_Name": "apple","Item Price":" 10$ "  }
+        ],
+        face=[r"D:\FinalProject\Final-BS.C-Project-2025\Data\Faces\Ali.png" , r"D:\FinalProject\Final-BS.C-Project-2025\Data\Faces\Mahdi.png" , r"D:\FinalProject\Final-BS.C-Project-2025\Data\Faces\Yazan.png"], 
+        time=datetime.now().strftime("%A, %Y-%m-%d %H:%M:%S")
+)
+#for the video, i think to upload the video to the youtube then put  the link of the video on the json then the owner of the store can open it when he resve the alert.
