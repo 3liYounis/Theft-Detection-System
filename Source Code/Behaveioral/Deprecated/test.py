d0019fb66521a802b269b9d8b6fbca60f60242e8
3liyounis <aliyounis642@gmail.com>
2025-10-17 00:15:01 +0300

    Untrack Previous Model Files (Deprecated)

--- Source Code/Behaveioral/Deprecated/test.py
+++
@@ -1,5 +0,0 @@
-# from detect_theft import detect_theft
-# normal_path = "./Data/Stream/Normal/Normal (85).mp4"
-# theft_path = "./Data/Stream/Shoplifting/Shoplifting (85).mp4"
-# info = detect_theft(normal_path)
-# print(info)
