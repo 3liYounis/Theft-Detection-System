08934ec3938deff7d974251ed6da4ee7cb578119
3liyounis <aliyounis642@gmail.com>
2025-10-17 01:07:16 +0300

    Delete Item Detection Model

--- Source Code/Alert/alert.py
+++ Source Code/Alert/alert.py
@@ -1,13 +1,12 @@
 import json
 
 
-def alert(theft_link, item, face, time):
+def alert(theft_link, face, time):
     theftalert = {
-        "theft link": theft_link,
-        "item": item,
-        "face ID": face,
-        "theft time": time
+        "Theft Link": theft_link,
+        "Face Info": face,
+        "Theft Time": time
     }
-    path = r"./Data/Alert.json"
+    path = "./../Data/Alert.json"
     with open(path, "w") as json_file:
         json.dump(theftalert, json_file, indent=4)
